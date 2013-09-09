836bae71244c95ca4f43298297844bf5b7494ff9
Mosquito-Lab <mosquito-lab@mosquitolab-DX4870.(none)>
2013-09-09 13:06:28 -0700

    printing progress

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -r jburkhar@aciss.uoregon.edu:/home13/jburkhar/research/Bradshaw/ --exclude 'output' --exclude 'reads' ./Bradshaw/
+rsync -r --progress jburkhar@aciss.uoregon.edu:/home13/jburkhar/research/Bradshaw/ --exclude 'output' --exclude 'reads' ./Bradshaw/
