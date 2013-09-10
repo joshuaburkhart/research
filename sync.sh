9dffa14313a545dafcb532c7adeba7516a66da38
Mosquito-Lab <mosquito-lab@mosquitolab-DX4870.(none)>
2013-09-10 15:11:38 -0700

    now pulling & pushing

--- sync.sh
+++ sync.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 rsync -r --progress jburkhar@aciss.uoregon.edu:/home13/jburkhar/research/Bradshaw/ --exclude 'output' --exclude 'reads' ./Bradshaw/
+rsync -r --progress ./Bradshaw/ jburkhar@aciss.uoregon.edu:/home13/jburkhar/research/Bradshaw/
