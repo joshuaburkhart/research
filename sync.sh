925da5ac5d0d179d19fbe3674644dd261745952a
Mosquito-Lab <mosquito-lab@mosquitolab-DX4870.(none)>
2013-09-13 08:38:23 -0700

    ..

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rsync -r --progress mosquito@shell.uoregon.edu:/home5/mosquito/public_html/ --exclude 'pages' --exclude '.gitignore' --exclude 'google68dd3e6184b6a0a6.html' --exclude 'index.html' --exclude 'index.html.under_construction_page' ./Bradshaw/
+rsync -r --progress mosquito@shell.uoregon.edu:/home5/mosquito/public_html/ --exclude 'pages' --exclude '.gitignore' --exclude 'google68dd3e6184b6a0a6.html' --exclude 'index.html' --exclude 'index.html.under_construction_page' ./Bradshaw/public_html/
 rsync -r --progress jburkhar@aciss.uoregon.edu:/home13/jburkhar/research/Bradshaw/ --exclude 'output' --exclude 'reads' ./Bradshaw/
 rsync -r --progress ./Bradshaw/ jburkhar@aciss.uoregon.edu:/home13/jburkhar/research/Bradshaw/
